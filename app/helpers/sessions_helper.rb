7dbc9f676ad83b9cad076bcec265a54d78e3399d
lihaozuo <lihaozuo@udesk.com>
2019-01-08 20:05:57 +0800

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,6 +24,11 @@ module SessionsHelper
     end
   end
 
+  #
+  def current_user?(user)
+    user == current_user
+  end
+
   # 如果用户已登录，返回 true，否则返回 false
   def logged_in?
     !current_user.nil?
@@ -48,5 +53,16 @@ module SessionsHelper
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
+  end
+
+  # 重定向到存储的地址或者默认地址
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # 存储后面需要使用的地址
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
